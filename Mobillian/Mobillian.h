e7f1431347f5781622118d7e8ea33c2d5a8b34de
ahmeturhan <ahmet.han@datailor.net>
2021-09-20 14:25:09 +0300

    Initial Commit

---
+++ Mobillian/Mobillian.h
@@ -0,0 +1,18 @@
+//
+//  Mobillian.h
+//  Mobillian
+//
+//  Created by Ahmet Han on 20.09.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Mobillian.
+FOUNDATION_EXPORT double MobillianVersionNumber;
+
+//! Project version string for Mobillian.
+FOUNDATION_EXPORT const unsigned char MobillianVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Mobillian/PublicHeader.h>
+
+
