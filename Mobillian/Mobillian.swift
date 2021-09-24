4f6c1a74e124e7214430af0cae94521398665684
ahmeturhan <ahmet.han@datailor.net>
2021-09-24 21:25:20 +0300

    add test cases

---
+++ Mobillian/Mobillian.swift
@@ -0,0 +1,20 @@
+//
+//  Mobillian.swift
+//  Mobillian
+//
+//  Created by Ahmet Han on 24.09.2021.
+//
+
+public final class Mobillian {
+
+    let name = "Mobillian"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
--- MobillianTests/MobillianTests.swift
+++ MobillianTests/MobillianTests.swift
@@ -10,6 +10,12 @@ import XCTest
 
 class MobillianTests: XCTestCase {
 
+    var mobillian: Mobillian!
+    
+    override func setUp() {
+        mobillian = Mobillian()
+    }
+
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -17,17 +23,20 @@ class MobillianTests: XCTestCase {
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
+    
+    func testAdd() {
+        XCTAssertEqual(mobillian.add(a: 1, b: 1), 2)
+    }
+    
+    func testSub() {
+        XCTAssertEqual(mobillian.sub(a: 2, b: 1), 1)
+    }
+ 
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+
 
 }
